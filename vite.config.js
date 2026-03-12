00ac31aaa7c4e62dd5bea09c8383d821aed41721
R.Richter@serrala.com <rainer.richter@nextcloudsolution.de>
2026-03-12 17:52:41 +0100

    App in Navigation

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        target: 'es2018',
-    },
     plugins: [
+        legacy({
+            targets: ['defaults', 'iOS >= 12'],
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
